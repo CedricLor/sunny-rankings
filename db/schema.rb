7d0bdef1f3ce346e7fac5c1d1ac02d6e0c0062f7
Cedric <cedric.lor@gmail.com>
2015-07-02 13:39:28 +0200

    changed timestamp of profile table to have it created before the user table

--- db/schema.rb
+++ db/schema.rb
@@ -105,14 +105,14 @@ ActiveRecord::Schema.define(version: 20150626152123) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.boolean  "validated",              default: false, null: false
-    t.integer  "user_id"
     t.string   "real_email"
+    t.integer  "profile_id"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["profile_id"], name: "index_users_on_profile_id"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["user_id"], name: "index_users_on_user_id"
 
 end
