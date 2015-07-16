a4ccb5b9dd16c0aded70a13d11c65b19d02f2d5b
Cedric <cedric.lor@gmail.com>
2015-07-16 20:11:57 +0200

    autodetect language params from header - very basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,15 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = extract_locale_from_accept_language_header || params[:locale] || I18n.default_locale
   end
 
   def default_url_options
     { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
   end
+
+  private
+    def extract_locale_from_accept_language_header
+      request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    end
 end
