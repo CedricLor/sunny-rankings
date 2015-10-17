910e1b02c8accc6607b0a5bae326be855687b574
Cedric <cedric.lor@gmail.com>
2015-10-17 17:22:32 +0200

    dual language for the legal warning

--- app/assets/javascripts/legal_warning.js
+++ app/assets/javascripts/legal_warning.js
@@ -5,26 +5,62 @@ $(window).load( function() {
   $("#legalWarning").addClass( "hidden" );
   /* create a const to generate legal warning */
   const num_of_issues = {
-          1: "a serious issue ",
-          2: "serious issues "
+          1: {
+            "en": "a serious issue of ",
+            "fr": "un problème sérieux sur la politique de lutte contre "
+          },
+          2: {
+            "en": "serious issues of ",
+            "fr": "des problèmes sérieux sur les politiques de lutte contre "
+          }
         },
         type_of_issue = {
-          3: "of wage discrimination. ",
-          4: "of sexual harassment. ",
-          7: "of wage discrimination and sexual harassment. "
+          3: {
+            "en": "wage discrimination. ",
+            "fr": "les discriminations de rémunération. "
+          },
+          4: {
+            "en": "sexual harassment. ",
+            "fr": "le harcélement sexuel. "
+          },
+          7: {
+            "en": "wage discrimination and sexual harassment. ",
+            "fr": "les discriminations de rémunération et contre le harcélement sexuel. "
+          }
         },
-        second_sentence = {
-          1: "this is a serious matter. ",
-          2: "these are serious matters. "
+        end_of_second_sentence = {
+          1: {
+            "en": "this is a serious matter. ",
+            "fr": "il s'agit d'affirmations graves. "
+          },
+          2: {
+            "en": "these are serious matters. ",
+            "fr": "il s'agit d'affirmations graves. "
+          }
+        },
+        beginning_of_warning = {
+          "en": "You are about to report ",
+          "fr": "Vous êtes sur le point de rapporter "
+        },
+        beginning_of_second_sentence = {
+          "en": "Please note that ",
+          "fr": "Veuillez noter qu'"
+        },
+        last_sentence = {
+          "en": "Please review our terms of use of service before doing so.",
+          "fr": "Merci de lire attentivement les termes et conditions avant de voter."
         }
   /* this function parses the url to identify the language preferences of the user */
   function get_language_preference() {
-    var url = window.location.href;
+    var language = "";
+    (/\/fr\//.test(window.location.href)) ? language = "fr" : language = "en";
+    return language;
   }
   /* this function generates the legal warning depending on which item have been selected */
   function legal_warning_builder(i, x) {
-    get_language_preference();
-    legal_warning_sentence = "You are about to report " + num_of_issues[i] + type_of_issue[x] + "Please note that " + second_sentence[i] + "Please review our terms of use of service before doing so.";
+    language = get_language_preference();
+    legal_warning_sentence = beginning_of_warning[language] + num_of_issues[i][language] + type_of_issue[x][language] + beginning_of_second_sentence[language] + end_of_second_sentence[i][language] + last_sentence[language];
+    return legal_warning_sentence
   }
   /* set of variables to be used for generating the legal warning */
   var i = 0,
